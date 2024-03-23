e695b16158930f942003d7088d3e83c9165021b1
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2024-03-23 16:02:28 +0100

    Face Create/Update commands with priority

--- ndn-cxx/mgmt/nfd/control-command.cpp
+++ ndn-cxx/mgmt/nfd/control-command.cpp
@@ -100,6 +100,7 @@ FaceCreateCommand::FaceCreateCommand()
 {
   m_requestValidator
     .required(CONTROL_PARAMETER_URI)
+    .optional(CONTROL_PARAMETER_PRIORITY)
     .optional(CONTROL_PARAMETER_LOCAL_URI)
     .optional(CONTROL_PARAMETER_FACE_PERSISTENCY)
     .optional(CONTROL_PARAMETER_BASE_CONGESTION_MARKING_INTERVAL)
@@ -109,6 +110,7 @@ FaceCreateCommand::FaceCreateCommand()
     .optional(CONTROL_PARAMETER_MASK);
   m_responseValidator
     .required(CONTROL_PARAMETER_FACE_ID)
+    .optional(CONTROL_PARAMETER_PRIORITY)
     .required(CONTROL_PARAMETER_URI)
     .required(CONTROL_PARAMETER_LOCAL_URI)
     .required(CONTROL_PARAMETER_FACE_PERSISTENCY)
@@ -141,6 +143,7 @@ FaceUpdateCommand::FaceUpdateCommand()
 {
   m_requestValidator
     .optional(CONTROL_PARAMETER_FACE_ID)
+    .optional(CONTROL_PARAMETER_PRIORITY)
     .optional(CONTROL_PARAMETER_FACE_PERSISTENCY)
     .optional(CONTROL_PARAMETER_BASE_CONGESTION_MARKING_INTERVAL)
     .optional(CONTROL_PARAMETER_DEFAULT_CONGESTION_THRESHOLD)
@@ -149,6 +152,7 @@ FaceUpdateCommand::FaceUpdateCommand()
     .optional(CONTROL_PARAMETER_MASK);
   m_responseValidator
     .required(CONTROL_PARAMETER_FACE_ID)
+    .optional(CONTROL_PARAMETER_PRIORITY)
     .required(CONTROL_PARAMETER_FACE_PERSISTENCY)
     .optional(CONTROL_PARAMETER_BASE_CONGESTION_MARKING_INTERVAL)
     .optional(CONTROL_PARAMETER_DEFAULT_CONGESTION_THRESHOLD)
--- tests/unit/mgmt/nfd/control-command.t.cpp
+++ tests/unit/mgmt/nfd/control-command.t.cpp
@@ -130,6 +130,7 @@ BOOST_AUTO_TEST_CASE(FaceUpdate)
   // Good request, bad response (Mask is forbidden but present)
   ControlParameters p2;
   p2.setFaceId(1)
+    .setPriority(2)
     .setFacePersistency(FACE_PERSISTENCY_PERSISTENT)
     .setBaseCongestionMarkingInterval(765_ns)
     .setDefaultCongestionThreshold(54321)
